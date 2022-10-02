2e3fe0d3ae4944c5fd256bb2a6406007431a2f12
JuanMValencia <valencia071996@gmail.com>
2022-10-01 20:53:46 -0500

    Create gestionar.php
    gestionar

---
+++ gestionar.php
@@ -0,0 +1,70 @@
+<?php
+    include('menu_superior.php');
+    include('menu_lateral.php');
+?>
+
+    <!-- leyenda inicio-->
+    <div class="height-100 bg-light container">
+        <div class="row">
+            <h2>Gestionar</h2>
+        </div>
+
+        <div class="d-flex bd-highlight mb-3">
+            <div class="p-2 bd-highlight">
+            </div>
+        </div>
+        <!--visual tabla -->
+        <div class="row">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">No. Solicitud</th>
+                        <th scope="col">Motivo</th>
+                        <th scope="col">Fecha Solicitud</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Visualizar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>0001</td>
+                        <td>Salud</td>
+                        <td>06/08/2021</td>
+                        <td>radicado</td>
+
+                        <td>
+                            <a href="radicar.php">
+                                <button type="button" class="btn btn-secondary">Gestionar</button>
+                            </a>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>0002</td>
+                        <td>Calamidad</td>
+                        <td>15/03/2022</td>
+                        <td>radicado</td>
+                        <td>
+                            <button type="button" class="btn btn-secondary">Gestionar</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>0003</td>
+                        <td>Otro</td>
+                        <td>28/06/2022</td>
+                        <td>radicado</td>
+                        <td>
+                            <button type="button" class="btn btn-secondary">Gestionar</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+    <!--Container Main end-->
