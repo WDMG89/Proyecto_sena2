aa3e0db79896375c33b668120a1e769ba3af7a87
sramirez02 <110506617+sramirez02@users.noreply.github.com>
2022-10-03 21:31:49 -0500

    Update verdoc.php
    actualización de botón para cancelar solicitud

--- verdoc.php
+++ verdoc.php
@@ -139,10 +139,36 @@ SERVICREDITO S.A.</h6>
     <br>
     <div class="row g-">
         <button type="button" class="w-100 btn btn-success btn-lg" data-bs-toggle="modal"
-            data-bs-target="#staticBackdrop">
+            data-bs-target="#staticBackdropverdoc">
             Modificar solicitud
         </button>
     </div>
+
+<!-- Modal -->
+                    <div class="modal fade" id="staticBackdropverdoc" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel"><b>Envio exitoso</b></h5>
+                                    <a href="solicitudes.php">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                    </a>
+                                </div>
+                                <div class="modal-body">
+                                    La solicitud se ha negado exitosamente
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="solicitudes.php">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Cerrar</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 <?php 
     include('piedepagina.php');
 ?>
